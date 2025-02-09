6df9537312057e443b8d754562b6f6b8c3b2b63c
Sudha Agrahari <117359974+sudhaagrahari20@users.noreply.github.com>
2025-02-10 00:00:47 +0530

    Create Minimum Integer.cpp

---
+++ Minimum Integer.cpp
@@ -0,0 +1,8 @@
+long long int sum=0;
+        int ans = INT_MAX;
+        sum = accumulate(A.begin(),A.end(),sum);
+        for(int i=0;i<N;i++){
+            if(sum<=(long long int)N*A[i])ans=min(ans,A[i]);
+        }
+        return ans;
+
